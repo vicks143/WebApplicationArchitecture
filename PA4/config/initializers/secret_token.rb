11d85dcaacfb7d9062ed8dfabeafe0f5dcac2e4c
vicks143 <johnvictor.a@sella.it>
2014-09-09 22:51:27 +0530

    Web Application Architecture
    Web Application Architecture

---
+++ PA4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '3661d0263ad8b1bb9addf5e2ec92c98c790210d909815653c4532a840a8161f9f761d2329941b745b082cf11053996977b1d88c967635cba1c70a676fa80392b'
---
+++ README.md
@@ -0,0 +1,13 @@
+WebAppArchitecture
+==================
+
+Coursera Web Application Architectures by Greg Heileman 2014 spring
+
+This repository contains:
+
+1) Quiz answer
+
+2) Programming Assignment
+
+for the course Web Application Architectures on Coursera
+
